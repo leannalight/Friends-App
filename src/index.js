7effad5ecf10f2371a8f47276f73d10f14ad944d
Anna Lee <annaleeart@yandex.ru>
2020-11-10 16:25:31 +0300

    added fonts

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {
 const buttonAuthorisation = document.querySelector('.button_type_authorisation');
 const buttonExit = document.querySelector('.button_type_exit');
 const friendContainer = document.querySelector('.friendlist');
-//const friendsCounter = document.querySelector('.friends__title');
 const vkRequest = 'https://oauth.vk.com/authorize?client_id=7653662&display=popup&redirect_uri=https://leannalight.github.io/friends-app/&scope=friends,status&response_type=token&v=5.52&state=123456';
 let token;
 let userID;
@@ -16,7 +15,6 @@ const friendList = new FriendList({
     container: friendContainer,
     blockName: 'friendlist'
 });
-
 
 function checkUrl() {
     let regexpToken = /(#access_token=)([a-z0-9]+)\&/g;
@@ -29,6 +27,7 @@ function checkUrl() {
             container.textContent = `${data.response[0].first_name} ${data.response[0].last_name}`;
             userPhoto.style.backgroundImage = `url('${data.response[0].photo_200}')`;
         });
+
      showFriends(token);
     }
 }
@@ -64,16 +63,10 @@ function vkLogout() {
         }
     });
 }
-/*
-function declination(number, titles) {
-    const cases = [2, 0, 1, 1, 1, 2];
-    return titles[ (number%100>4 && number%100<20)? 2:cases[(number%10<5)?number%10:5] ];
-}; */
 
 function showFriends(token) {
     sendRequest(`https://api.vk.com/method/friends.search?count=5&fields=photo_100&access_token=${token}&v=5.52`, function(data) {
         console.log(data);
-       // friendsCounter.textContent = `${data.response.count} ${declination(data.response.count, [' друг', ' друга', ' друзей'] )}`;
         friendList.render(data.response.items);
     });
 };
